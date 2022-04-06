4a2e74a3d856d6a29a51ce1dae0b5987c983f4a7
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-04-06 10:50:19 +0530

    testing options passed as array

---
+++ src/app/controllers/IndexController.php
@@ -0,0 +1,34 @@
+<?php
+
+use Phalcon\Mvc\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        if ($this->request->isPost()) {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, [
+                CURLOPT_URL => "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => [
+                    "X-RapidAPI-Host: matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+                    "X-RapidAPI-Key: 71be960f06msh887d16889105647p189daajsn7e0182a348c4",
+                    "accept: application/json"
+                ],
+            ]);
+            $response = json_decode(curl_exec($curl));
+            $this->view->data = $response;
+            /* echo '<pre>';
+            print_r($response);
+            die; */
+        }
+    }
+}
---
+++ src/public/index.php
@@ -0,0 +1,81 @@
+<?php
+
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Url;
+use Phalcon\Db\Adapter\Pdo\Mysql;
+use Phalcon\Config;
+use Phalcon\Events\Manager;
+
+
+$config = new Config([]);
+
+// Define some absolute path constants to aid in locating resources
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
+
+// Register an autoloader
+$loader = new Loader();
+
+$loader->registerDirs(
+    [
+        APP_PATH . "/controllers/",
+        APP_PATH . "/models/",
+    ]
+);
+
+$loader->register();
+
+$container = new FactoryDefault();
+
+$container->set(
+    'view',
+    function () {
+        $view = new View();
+        $view->setViewsDir(APP_PATH . '/views/');
+        return $view;
+    }
+);
+
+$container->set(
+    'url',
+    function () {
+        $url = new Url();
+        $url->setBaseUri('/');
+        return $url;
+    }
+);
+
+$factory  = new Phalcon\Config\ConfigFactory();
+$config = $factory->newInstance('php', APP_PATH . '/component/config.php');
+
+$eventManager = new Manager();
+
+$application = new Application($container);
+
+$container->set(
+    'db',
+    function () {
+        return new Mysql(
+            [
+                'host'     => 'mysql-server',
+                'username' => 'root',
+                'password' => 'secret',
+                'dbname'   => 'phalcon',
+            ]
+        );
+    }
+);
+
+try {
+    // Handle the request
+    $response = $application->handle(
+        $_SERVER["REQUEST_URI"]
+    );
+
+    $response->send();
+} catch (\Exception $e) {
+    echo 'Exception: ', $e->getMessage();
+}
